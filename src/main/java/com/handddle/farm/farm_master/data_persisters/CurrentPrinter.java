8611082e7e90265a5f8b630d346c141ae790ab0d
Pierre Marigo <=>
2021-11-03 00:45:36 +0100

    Minor bug fix

--- src/main/java/com/handddle/farm/farm_master/data_persisters/CurrentPrinter.java
+++
@@ -1,9 +0,0 @@
-package com.handddle.farm.farm_master.data_persisters;
-
-public class CurrentPrinter extends DataPersister {
-
-    public CurrentPrinter(String key, Object value) {
-        super(key, value);
-    }
-
-}
---
+++ src/main/java/com/handddle/farm/farm_master/data_persisters/CurrentPrinterDataPersister.java
@@ -0,0 +1,9 @@
+package com.handddle.farm.farm_master.data_persisters;
+
+public class CurrentPrinterDataPersister extends DataPersister {
+
+    public CurrentPrinterDataPersister(String key, Object value) {
+        super(key, value);
+    }
+
+}
