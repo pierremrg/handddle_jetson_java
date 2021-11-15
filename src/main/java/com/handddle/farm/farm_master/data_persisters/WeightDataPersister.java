43ceb1c46afa67768dee36517e5355becc87622d
Pierre Marigo <=>
2021-11-15 19:13:33 +0100

    Added WeightDataPersister

---
+++ src/main/java/com/handddle/farm/farm_master/data_persisters/WeightDataPersister.java
@@ -0,0 +1,9 @@
+package com.handddle.farm.farm_master.data_persisters;
+
+public class WeightDataPersister extends DataPersister {
+
+    public WeightDataPersister(String key, Object value) {
+        super(key, value);
+    }
+
+}
