16b62779fdffe2a64dfde399744c3ebeeacd3840
Pierre Marigo <=>
2021-11-29 23:12:15 +0100

    Added LedColorDataPersister

---
+++ src/main/java/com/handddle/farm/farm_master/data_persisters/LedColorDataPersister.java
@@ -0,0 +1,9 @@
+package com.handddle.farm.farm_master.data_persisters;
+
+public class LedColorDataPersister extends DataPersister {
+
+    public LedColorDataPersister(String key, Object value) {
+        super(key, value);
+    }
+
+}
